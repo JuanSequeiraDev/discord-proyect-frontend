33072373cb41f46b58d418c8a6e405e13e2a9d0c
juan.dev <juansequeira101@gmail.com>
2024-12-23 01:46:02 -0300

    vercel.json added

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,33 +1,64 @@
 import React, { useContext, useEffect, useState } from 'react'
 import './Sidenav.css'
 import { TbMessageCircleFilled } from "react-icons/tb";
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import { FaPlus } from "react-icons/fa";
 import { userContext } from '../context/UserContext';
+import { IoSettingsSharp } from "react-icons/io5";
+
 
 const Sidenav = () => {
-    const {userWorkspaces} = useContext(userContext)
+    const { userWorkspaces, userInfo } = useContext(userContext)
     const userWorkspacesObj = JSON.parse(userWorkspaces)
+    const userInfoObj = JSON.parse(userInfo)
+    const { pathname } = useLocation()
+    console.log(userInfoObj)
 
     return (
-        <nav className='sidenav'>
-            <NavLink className='chats-icon' to='/home'>
+        <>
+            <nav className='sidenav'>
+                <NavLink className='chats-icon' to='/home'
+                    style={pathname == '/home' ? { backgroundColor: '#5865F2' } : { backgroundColor: 'rgb(57, 62, 70)' }}>
                     <TbMessageCircleFilled className='msg-bubble' />
-            </NavLink>
-            {
-                userWorkspacesObj && userWorkspacesObj.map((workspace) => {
-                    const { workspace_img, workspace_id, workspace_name } = workspace
-                    return (
-                        <NavLink className='workspace-icon' to={'/workspace/' + workspace_id} key={workspace_id}>
+                </NavLink>
+                {
+                    userWorkspacesObj && userWorkspacesObj.map((workspace) => {
+                        const { workspace_img, workspace_id, workspace_name } = workspace
+                        return (
+                            <NavLink className='workspace-icon' to={'/workspace/' + workspace_id} key={workspace_id}>
                                 <img src={workspace_img} alt="img del workspace" className='workspace-img' />
-                        </NavLink>
-                    )
-                })
-            }
-            <NavLink to={'/create-workspace'} className='create-server-bubble'>
-                <FaPlus className='create-server-icon' />
-            </NavLink>
-        </nav>
+                            </NavLink>
+                        )
+                    })
+                }
+                <NavLink to={'/create-workspace'} className='create-server-bubble'>
+                    <FaPlus className='create-server-icon' />
+                </NavLink>
+            </nav>
+            <nav className='sidenav-full'>
+                <header className='visuals-header'>
+                    <div className='fake-input-box'>
+                        <div className='fake-input'>
+                            <span className='fake-input-txt'>Buscar o inciar conversacion</span>
+                        </div>
+                    </div>
+                    <div className='content-box'>
+                        <span className='content-txt'>Amigos</span>
+                    </div>
+                    <div className='content-box'>
+                        <span className='content-txt'>Nitro</span>
+                    </div>
+                    <div className='content-box'>
+                        <span className='content-txt'>Tienda</span>
+                    </div>
+                </header>
+                <footer className='user-config-footer'>
+                    <img className='user-config-pfp' src={userInfoObj.user_pfp} alt="imagen del usuario" />
+                    <span className='user-config-username'>{userInfoObj.name}</span>
+                    <IoSettingsSharp className='user-config-settings' />
+                </footer>
+            </nav>
+        </>
     )
 }
 
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from 'react'
 import './HomeScreen.css'
 import Sidenav from '../components/Sidenav'
 import UserChats from '../chats/UserChats'
+import { useLocation } from 'react-router-dom'
 
 const HomeScreen = () => {
+    
+
     return (
         <main className='main'>
             <Sidenav/>
